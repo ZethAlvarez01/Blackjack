60c1237f0e9c79cb193d34d73b305ea136acb700
ZethAlvarez01 <zetokaiba2000@hotmail.com>
2019-11-21 02:26:28 -0600

    Repartir baraja

--- Blackjack.cpp
+++ Blackjack.cpp
@@ -8,36 +8,47 @@ struct Carta
     char palo; // ♥ = C, ♦ = D, ♣ = T y ♠ = E
 };
 
-/* Baraja (Esta es una pila)*/
-struct Baraja{
+/* Pila (Esta es una pila)*/
+struct Pila{
     struct Carta *carta;
-    struct Baraja *siguiente;
+    struct Pila *siguiente;
 };
 
-/* Orden de turnos y jugadores (Esta es una lista simple) */
-struct Jugadores
+/*Cartas repartidas por jugador*/
+struct ListaCartas
+{
+	Carta* carta;
+	ListaCartas *siguiente;
+};
+
+/* Orden de turnos y Jugadores (Esta es una lista simple) */
+struct Lista
 {
 	char nombre[50];
     int apuesta;
-	struct Jugadores *siguiente;
+    int estado;                 // 0 = In Game 1 = Game Over
+    Carta* lista_de_cartas[10];
+	Lista *siguiente;
 };
 
-/*Operaciones para la pila (Baraja)*/
-int push(struct Baraja **baraja, Carta *carta);
-int pop(struct Baraja **baraja);
-void mostrarBaraja(struct Baraja *baraja);
 
-/*Operaciones para la lista (Jugadores)*/
-int agregar(struct Jugadores **turnos);
-int eliminar(struct Jugadores **turnos, char nombre[50]);
-void mostrarListaJugadores(struct Jugadores *turnos);
+/*Operaciones para la pila*/
+int push(struct Pila **Pila, Carta *carta);
+Pila* pop(struct Pila **Pila);
+void mostrarPila(struct Pila *Pila);
 
+/*Operaciones para la Lista*/
+void pushLista(Lista* &);
+void popLista(Lista* &,char []);
+void mostrarLista(Lista*,int);
+int tamanoLista(Lista*);
+void pushCrupier(Lista* &);
 
-Carta* CrearBaraja(Carta *ArregloBaraja[]);
-Carta* Barajar(Carta *ArregloBaraja[]);
-void Crear_Barajar_Y_Anadir(struct Baraja **baraja);
+Carta* CrearPila(Carta *ArregloPila[]);
+Carta* Pilar(Carta *ArregloPila[]);
+void Crear_Pilar_Y_Anadir(struct Pila **Pila);
 
-int tamano(struct Jugadores *lista){                                     // Paso por valor
+int tamano(struct Lista *lista){                                     // Paso por valor
                                                                         // Apuntador auxiliar que comienza donde inicia la lista (No es necesario XD trabaja con lista)
     int pos=0;
     while(lista->siguiente!=NULL){                                        // Mientras aux->siguiente sea diferente de nulo (Recorre toda la lista)
@@ -60,37 +71,169 @@ void titulo(){
     printf("****           Se paga 2 a 1            ****\n\n");
 }
 
+void anadirjugadores(Lista* &orden_turnos){
+    system("cls");
+
+    titulo();
+
+    printf("Anadir jugadores\n");
+    int stop=1;
+    do{
+        printf("Anade el nombre del nuevo jugador\n");
+        pushLista(orden_turnos);
+        printf("Anadir mas jugadores? [1/YES][0/No]\n");
+        scanf("%d",&stop);
+    }while(stop!=0);
+}
 
 int main(){
 
-    struct Baraja *baraja=NULL;                                         /* Se crea el apuntador de la Baraja (Pila) */
+    struct Pila *Pila=NULL;                                         /* Se crea el apuntador de la Pila (Pila) */
+	Pila=(struct Pila*)malloc(sizeof(struct Pila));               /* Se asigna espacio en memoria */
+	Pila->siguiente=NULL;                                             /* Se asigna siguiente a NULL por que la pila esta vacia */
+
+    Lista *orden_turnos=NULL;                                /* Se crea el apuntador de la lista (Orden de turnos) */
+    orden_turnos=(struct Lista*)malloc(sizeof(struct Lista));   /* Se asigna espacio en memoria */
+
+    Crear_Pilar_Y_Anadir(&Pila);   
+
+    system("cls");
+
+    titulo(); 
+
+    anadirjugadores(orden_turnos);
+
+    int control=0;
+    do{
+        system("cls");
+        titulo();
+        printf("Comienza el juego!\nLista de jugadores: \n");
+        mostrarLista(orden_turnos,1);
+        printf("\nEmpezar o anadir otro jugador? [0 Empezar][1 Anadir jugador]\n");
+        int op=0;
+        scanf("%d",&op);
+        switch (op)
+        {
+        case 0:
+            control=1;
+            break;
+        case 1:
+            anadirjugadores(orden_turnos);
+            break;
+        default:
+            break;
+        }
+
+    }while(control==0);
+
+    pushCrupier(orden_turnos);
+
+    /*Comienza el juego
+        Fases del turno
+        1° Se hacen apuestas
+        2° Se reparten las cartas a todos
+        3° Inician jugadores
+            - Separa
+            - Dobla
+            - Pide o planta
+        4° Juega el crupier
+        5° Se pagan apuestas
+    */
+
+   /* 1° Se hacen apuestas */
+    system("cls");
+    titulo();
+    printf("\nHagan sus apuestas\n");
+    mostrarLista(orden_turnos,0);
+    Lista *aux2=orden_turnos;
+    int apuestas[tamanoLista(orden_turnos)-1];
+    int pos=0;
+
     
-	baraja=(struct Baraja*)malloc(sizeof(struct Baraja));               /* Se asigna espacio en memoria */
+    while(aux2->siguiente!=NULL){
+        printf("%s tienes para apostar: %d creditos\n",aux2->nombre,aux2->apuesta);
+        int flg=1;
+        do{
+                
+                printf("Cuanto quieres apostar?\n");
+                int apu_actual=0;
+                scanf("%d",&apu_actual);
+                if(apu_actual > aux2->apuesta){
+                    printf("No puedes apostar mas de lo que tienes.\n");
+                    flg=1;
+                }else{
+                    flg=0;
+                    apuestas[pos]=apu_actual;
+                    pos++;
+                }
+        }while(flg!=0);
+        
+        aux2=aux2->siguiente;
+    }
+
+    for(int i=0;i<tamanoLista(orden_turnos)-1;i++){
+        printf("%d ",apuestas[i]);
+    }
+
+    /* 2° Se reparten las cartas a todos */
+    system("cls");
+    titulo();
+    printf("\nReparticion de cartas\n");
+    printf("Las cartas se repartieron asi de la siguete forma.\n");
+    mostrarPila(Pila);   
+
+    Lista* aux5=orden_turnos;
+    ListaCartas* mano;
+    while(aux5->siguiente!=NULL){
+        printf("%s\n",aux5->nombre);
+        mano=NULL;
+        struct Pila* carta=pop(&Pila);
+        printf("%d%c\n",carta->carta->valor,carta->carta->palo);
+        aux5->lista_de_cartas[0]=carta->carta;
+        carta=pop(&Pila);
+        printf("%d%c\n",carta->carta->valor,carta->carta->palo);
+        aux5->lista_de_cartas[1]=carta->carta;
+        aux5=aux5->siguiente;
+    }
+    printf("%s\n",aux5->nombre);
+    mano = NULL;
+    struct Pila* carta=pop(&Pila);
+    printf("%d%c\n",carta->carta->valor,carta->carta->palo);
+    aux5->lista_de_cartas[0]=carta->carta;
+    carta=pop(&Pila);
+    printf("%d%c\n",carta->carta->valor,carta->carta->palo);
+    aux5->lista_de_cartas[1]=carta->carta;
+
+    Lista* aux6=orden_turnos;
+     while(aux6->siguiente!=NULL){
+         printf("La mano de %s es: \n",aux6->nombre);
+         Carta* mano=aux6->lista_de_cartas[0];
+         printf("%d%c ",mano->valor,mano->palo);
+         mano=aux6->lista_de_cartas[1];
+         printf("%d%c \n",mano->valor,mano->palo);
+         aux6=aux6->siguiente;
+     }
+     printf("La mano del %s es: \n",aux6->nombre);
+     Carta* mano2=aux6->lista_de_cartas[0];
+     printf("%d%c ",mano2->valor,mano2->palo);
+     mano2=aux6->lista_de_cartas[1];
+     printf("%d%c \n",mano2->valor,mano2->palo);
+
+
+    //mostrarPila(Pila);  
+
+
     
-	baraja->siguiente=NULL;                                             /* Se asigna siguiente a NULL por que la pila esta vacia */
 
-    struct Jugadores *orden_turnos=NULL;                                /* Se crea el apuntador de la lista (Orden de turnos) */
 
-    //orden_turnos=(struct Jugadores*)malloc(sizeof(struct Jugadores));   /* Se asigna espacio en memoria */
-                                        
-    Crear_Barajar_Y_Anadir(&baraja);                                    /* Se Crea, barajan las cartas y se meten a la pila para simular la baraja*/
+    
 
-    printf("%d \n",tamano(orden_turnos));
-    agregar(&orden_turnos);
-    agregar(&orden_turnos);
-    agregar(&orden_turnos);
-    printf("%d \n",tamano(orden_turnos));
 
-    printf("1-----\n");
-    mostrarListaJugadores(orden_turnos);    
-    printf("-----\n");
-
-    titulo();                                                          /* Imprime el bonito titulo */
 
     return 0;
 }
 
-Carta* Barajar(Carta *ArregloBaraja[]){
+Carta* Pilar(Carta *ArregloPila[]){
     Carta *aux;
     aux=(struct Carta*)malloc(sizeof(struct Carta));
     int random;
@@ -98,29 +241,29 @@ Carta* Barajar(Carta *ArregloBaraja[]){
         for(int i = 0; i < 52; i++)
         {
             random = rand()%52;
-            aux = ArregloBaraja[i];
-            ArregloBaraja[i]=ArregloBaraja[random];
-            ArregloBaraja[random]=aux;
+            aux = ArregloPila[i];
+            ArregloPila[i]=ArregloPila[random];
+            ArregloPila[random]=aux;
         }
     }
 
-    return *ArregloBaraja;
+    return *ArregloPila;
 }
 
-void Crear_Barajar_Y_Anadir(struct Baraja **baraja){
-    Carta *ArregloBaraja[52];
+void Crear_Pilar_Y_Anadir(struct Pila **Pila){
+    Carta *ArregloPila[52];
     
-    *ArregloBaraja=CrearBaraja(ArregloBaraja);
+    *ArregloPila=CrearPila(ArregloPila);
     
-    *ArregloBaraja=Barajar(ArregloBaraja);
+    *ArregloPila=Pilar(ArregloPila);
     
     for(int i=0;i<52;i++){
-        push(&*baraja, ArregloBaraja[i]);
+        push(&*Pila, ArregloPila[i]);
     }
 
 }
 
-Carta* CrearBaraja(Carta *ArregloBaraja[]){
+Carta* CrearPila(Carta *ArregloPila[]){
     int indice=0;
     for(int i=0;i<4;i++){
             for(int j=0;j<13;j++){
@@ -148,18 +291,18 @@ Carta* CrearBaraja(Carta *ArregloBaraja[]){
                 default:
                     break;
                 }
-                ArregloBaraja[indice]=carta;
+                ArregloPila[indice]=carta;
                 indice++;
             }        
         }
-        return *ArregloBaraja;
+        return *ArregloPila;
 }
 
-int push(struct Baraja **baraja, Carta *carta)
+int push(struct Pila **Pila, Carta *carta)
 {
-	struct Baraja *nuevo=NULL;
+	struct Pila *nuevo=NULL;
 
-	nuevo=(struct Baraja*)malloc(sizeof(struct Baraja));
+	nuevo=(struct Pila*)malloc(sizeof(struct Pila));
 	
 	if(nuevo==NULL)
 	{
@@ -167,158 +310,156 @@ int push(struct Baraja **baraja, Carta *carta)
 	}
 
 	nuevo->carta=carta;
-	nuevo->siguiente=*baraja;
-	*baraja=nuevo;
+	nuevo->siguiente=*Pila;
+	*Pila=nuevo;
 
 	return 0;
 }
 
-int pop(struct Baraja **baraja)
+Pila* pop(struct Pila **Pila)
 {
-	struct Baraja *aux=NULL;
-	aux=*baraja;
+	struct Pila *aux=NULL;
+	aux=*Pila;
 
-	if(*baraja==NULL)
+	if(*Pila==NULL)
 	{
-		return -1;
+		return NULL;
 	}
 	else
 	{
-		*baraja=aux->siguiente;
-		free(aux);
+		*Pila=aux->siguiente;
+        return aux;
 	}
-	
-	return 0;
 }
 
-void mostrarBaraja(struct Baraja *baraja)
+void mostrarPila(struct Pila *Pila)
 {
-	while(baraja->siguiente!=NULL)
+	while(Pila->siguiente!=NULL)
 	{
-            if( (baraja->carta)->valor <= 10){
-                printf("%d%c ",(baraja->carta)->valor,(baraja->carta)->palo);
+            if( (Pila->carta)->valor <= 10){
+                printf("%d%c ",(Pila->carta)->valor,(Pila->carta)->palo);
             }else{
-                if((baraja->carta)->valor == 11){
-                    printf("J%c ",(baraja->carta)->palo);
+                if((Pila->carta)->valor == 11){
+                    printf("J%c ",(Pila->carta)->palo);
                 }
-                if((baraja->carta)->valor == 12){
-                    printf("Q%c ",(baraja->carta)->palo);
+                if((Pila->carta)->valor == 12){
+                    printf("Q%c ",(Pila->carta)->palo);
                 }
-                if((baraja->carta)->valor == 13){
-                    printf("K%c ",(baraja->carta)->palo);
+                if((Pila->carta)->valor == 13){
+                    printf("K%c ",(Pila->carta)->palo);
                 }
             }
-            baraja=baraja->siguiente;
+            Pila=Pila->siguiente;
 	}
 	printf("\n");
 }
 
-int agregar(struct Jugadores **lista)
-{
-	struct Jugadores *nuevo=NULL;
-	nuevo=(struct Jugadores*)malloc(sizeof(struct Jugadores));
-
-    struct Jugadores *actual=NULL;
-	actual=(struct Jugadores*)malloc(sizeof(struct Jugadores));
-    actual->siguiente=NULL;
-    actual=*lista;
-
-    struct Jugadores *siguiente=NULL;
-	siguiente=(struct Jugadores*)malloc(sizeof(struct Jugadores));
-    siguiente->siguiente=NULL;
-    siguiente=*lista;
-
-	if(nuevo==NULL)
-	{
-		return 0;
-	}
-
+void pushLista(Lista* &lista){                            // Paso por referencia
+    Lista* nuevo;                                  // nodo* nuevo=new nodo();
+    nuevo=(struct Lista*)malloc(sizeof(struct Lista));
     printf("Nombre: ");
     scanf("%s",nuevo->nombre);
 
     nuevo->apuesta=100;
+    nuevo->estado=0;       
+    nuevo->siguiente=NULL;     
 
-	if(*lista==NULL){                                        // Si la lista esta vacia
-        *lista=nuevo;
-    }else{                                                  // Si no esta vacia   
+    if(lista==NULL){                                        // Si la lista esta vacia
+        lista=nuevo;
+        printf("Lista estaba vacia");              
+        printf("Jugador %s anadido\n",nuevo->nombre);
+    }else{                                                  // Si no esta vacia                                  
+        Lista* sig=lista;                                     // Dos apuntadores auxiliares
+        Lista* actual=lista;                                  // Uno a el elemento actual y otro al siguiente (mayor y menor)
         
-        while(siguiente!=NULL){                             // Busco en la lista la posicion en la que va nuevo (entre actual y sig)
-            actual=siguiente;
-            siguiente=siguiente->siguiente;
+        while((sig!=NULL)){                                    
+            actual=sig;
+            sig=sig->siguiente;
         }
-                                                            // Si no entra al while o termina, ya encontro la posición en donde va nuevo (entre actual y sig) 
-        if(actual==siguiente){                                    // Si actual y sig son iguales entonces nuevo va primero en la lista
-            nuevo->siguiente=*lista;                     
-            *lista=nuevo;    
+                                                            
+        if(actual==sig){                                    
+            nuevo->siguiente=lista;                     
+            lista=nuevo;    
         }else{
             actual->siguiente=nuevo;                        // En caso contrario va entre elementos y se inserta despues de actual y antes de siguiente
-            nuevo->siguiente=siguiente;
+            nuevo->siguiente=sig;
         }
-       
+        printf("Jugador %s anadido\n",nuevo->nombre);
     }
-
-
-	return 0;
 }
 
-int eliminar(struct Jugadores **lista,  char nombre[50])
-{
-	struct Jugadores *actual=NULL;
-	struct Jugadores *aux=NULL;
-	struct Jugadores *primero=NULL;
-	actual=*lista;
-	aux=*lista;
-	primero=(*lista)->siguiente;
-
-	if((*lista)->nombre==nombre)
-	{
-		while(aux->siguiente!=*lista)
-		{
-			aux=aux->siguiente;
-		}
-		
-		aux->siguiente=(*lista)->siguiente;
-		free(*lista);
-		*lista=aux;
-	}
-	else
-	{
-		while(primero!=*lista)
-		{
-			if(actual->nombre!=nombre)
-			{
-				actual=actual->siguiente;
-			}
-		
-			if(aux->siguiente!=actual)
-			{
-				aux=aux->siguiente;
-			}
-
-			primero=primero->siguiente;	
-		}
-
-		aux->siguiente=actual->siguiente;
-		free(actual);
-		actual=*lista;
-		aux=*lista;
-	}	
-
-	return 0;
-}
-
-void mostrarListaJugadores(struct Jugadores *lista)
-{
-
+void popLista(Lista* &lista,char nombre[50]){
     if(lista==NULL){
-        printf("Lista vacia\n");
+        printf("\nLista vacia");
     }else{
-        while(lista->siguiente!=NULL){                                                        // Mientras aux->siguiente sea diferente de nulo va a imprimir el elemento
-            printf("Nombre: %s Apuesta: %d\n", lista->nombre,lista->apuesta);
-            lista=lista->siguiente;                                                             // Imprime y aux avanza al siguente
+        Lista* sig=lista;                                     // Dos apuntadores auxiliares
+        Lista* actual=lista;                                  // Uno a el elemento actual y otro al siguiente (mayor y menor)
+        
+        while((sig!=NULL)&&(lista->nombre!=nombre)){              // Busco en la lista la posicion en la que va nuevo (entre actual y sig)
+            actual=sig;
+            sig=sig->siguiente;
         }
-        printf("Nombre: %s Apuesta: %d\n", lista->nombre,lista->apuesta);                       // Imprime el ultimo elemento de la lista
+
+        if(sig==NULL){                                              // Ultimo elemento y sin coincidencias
+            printf("\nEste jugador %s no esta jugando\n",nombre);
+        }
+
+        if(actual==sig){                                            // El elemento a eliminar esta primero
+            lista=sig->siguiente;
+            printf("\nElemento %s eliminado\n",nombre);
+        }else{                                                      // El elemento a eliminar es cualquiera de los otros
+            actual->siguiente=sig->siguiente;
+            printf("\nElemento %s eliminado\n",nombre);
+        }
+    }
+}
+
+void mostrarLista(Lista* lista, int flg){     
+    if(flg==1){
+        lista=lista->siguiente;
+    }
+    Lista* aux=lista;                                         // Apuntador auxiliar que comienza donde inicia la lista (No es necesario XD trabaja con lista)
+    if(lista==NULL){
+        printf("\nLista vacia\n");
+    }else{
+        while(aux->siguiente!=NULL){                        // Mientras aux->siguiente sea diferente de nulo va a imprimir el elemento
+            printf("- %s\n",aux->nombre);
+            aux=aux->siguiente;                             // Imprime y aux avanza al siguente
+        }
+        printf("- %s\n",aux->nombre);                            // Imprime el ultimo elemento de la lista
     }
 
 
-}	
+}
+
+int tamanoLista(Lista* lista){                                     // Paso por valor
+    Lista* aux=lista;                                         // Apuntador auxiliar que comienza donde inicia la lista (No es necesario XD trabaja con lista)
+    int pos=0;
+    while(aux->siguiente!=NULL){                            // Mientras aux->siguiente sea diferente de nulo (Recorre toda la lista)
+        aux=aux->siguiente;                                 // Aumenta el numero de elementos y pasa al siguiente
+        pos++;
+    }
+    pos++;                                                  // Ultimo elemento
+    
+    return pos;
+}
+
+void pushCrupier(Lista* &lista){
+
+   char banca[8]="Crupier";
+   for(int i=0;i<7;i++){
+       lista->nombre[i]=banca[i];
+   }
+   lista->apuesta=1000;
+   lista->estado=0;                                          
+   //printf("Jugador %s anadido\n",lista->nombre);
+
+   Lista* aux=lista;
+   lista=lista->siguiente;
+   Lista* aux2=lista;
+   while(aux2->siguiente!=NULL){
+       aux2=aux2->siguiente;
+   }
+   aux->siguiente=NULL;
+   aux2->siguiente=aux;
+}
